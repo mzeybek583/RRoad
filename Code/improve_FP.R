375eae4144cc037d4e26b9a22551f5d58b80b1d6
mzeybek583 <muzo583@gmail.com>
2020-11-30 00:35:51 +0300

    comments on code

--- Code/improve_FP.R
+++ Code/improve_FP.R
@@ -7,15 +7,20 @@
 library(lidR)
 
 # Load Data ---------------------------------------------------------------
-las <- readLAS(files = "data/improve_FP/road.las")
+las <- readLAS(files = "data/improve_FP/road.las") 
 
-plot(las)
+#plot(las)
 
-metrics <- point_metrics(las, ~list(zdev = sd(Z)), k = 7, r = 3) # 3
+metrics <- point_metrics(las, ~list(zdev = sd(Z)), k = 7, r = 3) # knn based sd
 
-las <- add_attribute(las, metrics$zdev, "Zdev")
+las <- add_attribute(las, metrics$zdev, "Zdev") # add metrics into our las file
 
-plot(las2, color="Zdev", legend=T)
+#plot(las2, color="Zdev", legend=T)
 
-writeLAS(las2, "Zdev.las")
+Zdev_free = filter_poi(las2, Zdev <=0.005) # filter outside of deviation of 005
+Zdev_out = filter_poi(las2, Zdev >0.005)  # filtered outlier points
+
+#plot(Zdev_free, color="Zdev", legend=T)
+
+writeLAS(Zdev_free, "Zdev.las") # write to las file
   
