021af02b9fd6d2a123b1979040602ed1a1387cf5
mzeybek <muzo583@gmail.com>
2021-04-25 18:41:07 +0300

    Update adaptive_thresholding.R

--- Code/adaptive_thresholding.R
+++ Code/adaptive_thresholding.R
@@ -2,12 +2,18 @@
 
 #Adaptive thresholding
 
+
+# Set maximum memory allocation -------------------------------------------
+
+
 memory.size(max = TRUE)
 memory.limit(size=56000)
 
 # Load libraries ----------------------------------------------------------
 
 library(lidR)
+library(RANN)
+library(ggplot2)
 
 time <- proc.time()
 
@@ -15,6 +21,9 @@ time <- proc.time()
 data <- readLAS(files = "test4.las")
 
 proc.time() - time
+
+
+
 # New grayscale image = ( (0.3 * R) + (0.59 * G) + (0.11 * B) ).
 # Agisoft formula: 0.21 R + 0.72 G + 0.07 B.
 d <- data@data
@@ -40,14 +49,25 @@ plot(las.export, color="Intensity", legend=T)
 
 #xx <- readLAS("Road_intensity.las")
 
+# Smoth Intensity ---------------------------------------------------------
+smooth_i <- function(x) { # user-defined function
+   m <- median(x)
+   return(m) # output
+}
+
+metrics <- point_metrics(las.export, ~smooth_i(Intensity), k = 6) # 1
+
+las.export@data$Intensity <- metrics$V1
+
+plot(las.export, color="Intensity", legend=T)
+
 # Compute Neighbouring ----------------------------------------------------
 
-library(data.table)
-library(RANN)
+
 
 df <- las.export@data
 
-k <- 20
+k <- 20 # Neighboor points
 
 dist <- nn2(df[,1:3],df[,1:3], k=k+1, "kd")
 
@@ -57,37 +77,37 @@ df$road <- 0
 
 # Histogram of Intensity --------------------------------------------------
 
+#Normalize Intensity Value between 0-1
+
 normalize <- function(x) {
    return ((x - min(x)) / (max(x) - min(x)))
 }
-
 df$Intensity <- normalize(df$Intensity)
-
 summary(df$Intensity)
-
 tj <- nrow(df)
 # create progress bar
 pb <- txtProgressBar(min = 0, max = tj, style = 3)
 
+# Adaptive Threshold According to std around "k" points -------------------
+
 for (j in 1:tj) {
-  
 #hist(df[dist$nn.idx[2:k]]$int)
 std <- sd(df[dist$nn.idx[2:k]]$Intensity)
 mm <- median(df[dist$nn.idx[2:k]]$Intensity)
 
-sigma <- 2
+sigma <- 1 # change sigma if require
 
 cond.max <- mm + sigma*std
 cond.min <- mm - sigma*std
 
-if (df[dist$nn.idx[j]]$Intensity  > 0.6) {
- #  road <- ifelse(df[dist$nn.idx[j]]$Intensity > cond.min, 1,0)
-  road <- ifelse(df[dist$nn.idx[j]]$Intensity > cond.min && df[dist$nn.idx[j]]$Intensity < cond.max, 0,1)
 
-    #road <- df[,which(df[dist$nn.idx[j]]$Intensity> 0.6)]
-} else{
+if (df[dist$nn.idx[j]]$Intensity  > 0.6) { # Change parameter 0.6 if require
+  road <- ifelse(df[dist$nn.idx[j]]$Intensity > cond.min && df[dist$nn.idx[j]]$Intensity < cond.max, 0, 1)
+  } else{
   road <- 0
 }
+
+## Filter Certain Intensity Value
 
  # if (df[dist$nn.idx[j]]$Intensity  < 0.6) {
  #    road <- 0
@@ -95,20 +115,16 @@ if (df[dist$nn.idx[j]]$Intensity  > 0.6) {
  #    road <- 1
  # }
 
-
-   df[dist$nn.idx[j]]$road <- road
-
+df[dist$nn.idx[j]]$road <- road
 setTxtProgressBar(pb, j)
-
 }
-proc.time() - time
-
 close(pb)
 
+proc.time() - time
 
+
+# Plot on R ---------------------------------------------------------------
 df$road <- as.factor(df$road)
-
- library(ggplot2)
  ggplot(df, aes(X, Y,color= road))+
    geom_point()+
    scale_colour_manual(values=c("grey", "red"))+
@@ -117,10 +133,14 @@ df$road <- as.factor(df$road)
    theme_bw(base_size = 20)
 
  df$road <- as.integer(df$road)
- length(which(df$road==1))
- length(which(df$road==0))
+ sprintf("Founded  %i points roadlane",  length(which(df$road==1)))# Non-roadlane
+ sprintf("Founded  %i points roadlane",   length(which(df$road==2)))## roadlane
  
  
  sprintf("Process ending in %3.1f s", (proc.time() - time)[3])
+ 
+
+# Export File -------------------------------------------------------------
+
  
 write.csv(cbind(df[,1:3],intensity=df$Intensity, road=df$road),"result.csv")
